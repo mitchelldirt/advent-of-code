27e04d85b3fceb5a1e0a6f48eb44afa6d6cc071c
Mitchell Mudd <mitchellmudd@outlook.com>
2022-12-04 12:34:42 -0500

    Finished day 4 of advent of code - took me a bit to get the first part and then less than two minutes on the second part

--- day3/day3-part1.js
+++ day3/day3-part1.js
@@ -51,3 +51,4 @@ console.log('A'.charCodeAt(0))
 
 
 
+console.log(input)
---
+++ day4/day4-part1.js
@@ -0,0 +1,83 @@
+// GET FILE INPUT
+const fs = require('fs');
+
+function readFile(filePath) {
+  try {
+    const data = fs.readFileSync(filePath);
+    return data.toString();
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
+}
+
+const input = readFile('./day4/day4Input.txt');
+// END OF GET FILE INPUT
+
+// My methodology ------------------------------
+
+// split by line
+
+
+// Turn num1-num2 into an array of all numbers from num1...num2
+
+// for the larger array perform a loop: largerArray.includes(item from smaller array) if any of them return false 
+// Then you can move onto the next pair. If you only get true then add 1 to the resulting number
+
+
+// -1 until less than first number then return the array
+
+// SOLUTION ------------------------------------
+
+const totalContainedRanges = (inputString) => {
+  const inputArray = inputString.split('\n');
+  let total = 0;
+
+  for (let pair of inputArray) {
+    const pairArray = pair.split(',')
+    const range1 = createRange(pairArray[0]);
+    const range2 = createRange(pairArray[1]);
+
+    total += isOverlapping(range1, range2)
+  }
+
+  return total
+}
+
+const createRange = (rangeString) => {
+  let resultArray = [];
+  let splitNumbers = rangeString.split('-');
+
+  for (let i = +splitNumbers[1]; i >= splitNumbers[0]; i--) {
+    resultArray.push(i);
+  }
+
+  return resultArray.reverse();
+}
+
+const isOverlapping = (array1, array2) => {
+  let biggerArray;
+  let smallerArray;
+
+  const array1Length = array1.length;
+  const array2Length = array2.length;
+
+  if (array1Length > array2Length) {
+    biggerArray = array1;
+    smallerArray = array2;
+  } else if (array1Length < array2Length) {
+    biggerArray = array2;
+    smallerArray = array1;
+  } else {
+    biggerArray = array1;
+    smallerArray = array2
+  }
+
+  for (let item of smallerArray) {
+    if (biggerArray.includes(item) === false) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+console.log(totalContainedRanges(input))
